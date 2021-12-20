5835efef17c73266964aaf4c17ddaaea1607274b
imran-prog <infotainer04@gmail.com>
2021-12-20 23:58:00 +0500

    Completed Shop page with navigation working and vreated Info section for shop page

--- src/app/shop/deals/deals.component.ts
+++ src/app/shop/deals/deals.component.ts
@@ -1,15 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Renderer2, Inject } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'app-deals',
   templateUrl: './deals.component.html',
-  styleUrls: ['./deals.component.css']
+  styleUrls: ['./deals.component.css'],
 })
 export class DealsComponent implements OnInit {
-
-  constructor() { }
+  constructor(
+    private _renderer2: Renderer2,
+    @Inject(DOCUMENT) private _document: Document
+  ) {}
 
   ngOnInit(): void {
+    let script = this._renderer2.createElement('script');
+    script.type = 'text/javascript';
+    script.text = `
+    function openNav(evt, navName) {
+      var i, tabcontent, tablinks;
+      tabcontent = document.getElementsByClassName("tabcontent");
+      for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+      }
+      tablinks = document.getElementsByClassName("tablinks");
+      for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+      }
+      document.getElementById(navName).style.display = "block";
+      evt.currentTarget.className += " active";
+    }
+    
+    // Get the element with id="defaultOpen" and click on it
+    document.getElementById("defaultOpen").click();
+    `;
+    this._renderer2.appendChild(this._document.body, script);
   }
-
 }
