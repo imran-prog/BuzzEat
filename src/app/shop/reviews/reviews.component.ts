259aa17becf2f6a960d58eb280a723d9fd85beba
imran-prog <infotainer04@gmail.com>
2021-12-20 22:33:06 +0500

    Added review component in shop page

--- src/app/shop/reviews/reviews.component.ts
+++ src/app/shop/reviews/reviews.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Renderer2, Inject } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'app-reviews',
@@ -7,9 +8,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReviewsComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private _renderer2: Renderer2,
+    @Inject(DOCUMENT) private _document: Document
+  ) { }
 
   ngOnInit(): void {
+    let script = this._renderer2.createElement('script');
+    script.type = 'text/javascript';
+    script.text = `
+    var slider = document.getElementById("stars");
+    var output = document.getElementById("rating");
+    output.innerHTML = slider.value; // Display the default slider value
+
+    // Update the current slider value (each time you drag the slider handle)
+    slider.oninput = function() {
+      output.innerHTML = this.value;
+    }
+    `;
+    this._renderer2.appendChild(this._document.body, script);
   }
 
 }
