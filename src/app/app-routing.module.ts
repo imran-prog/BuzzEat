1fcc4f6bdf051777acf4dea8fb5d22a5f6314599
imran-prog <infotainer04@gmail.com>
2021-12-28 21:31:34 +0500

    Created location base products component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,16 +9,24 @@ import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
 import { PrivacyComponent } from './privacy/privacy.component';
 import { LocationsComponent } from './locations/locations.component';
+import { Error404Component } from './error404/error404.component';
+import { PayoutComponent } from './payout/payout.component';
+import { LocationBaseComponent } from './restaurant/location-base/location-base.component';
+import { ProductBaseComponent } from './restaurant/product-base/product-base.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'shop', component: ShopComponent },
   { path: 'login', component: LoginComponent },
+  { path: 'nearme/pizza', component: ProductBaseComponent },
+  { path: 'nearme/islamabad', component: LocationBaseComponent },
   { path: 'register', component: SignupComponent },
   { path: 'about', component: AboutComponent },
   { path: 'contact', component: ContactComponent },
   { path: 'privacy', component: PrivacyComponent },
   { path: 'locations', component: LocationsComponent },
+  { path: 'payout', component: PayoutComponent },
+  { path: '**', component: Error404Component },
 ];
 
 @NgModule({
